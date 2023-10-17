a102b0746f5de8b7c968efb735d6f7404fba6639
gg3ry <148282042+gg3ry@users.noreply.github.com>
2023-10-17 22:41:47 +0200

    Create cd.js

---
+++ scripts/cd.js
@@ -0,0 +1,15 @@
+var countDownDate = new Date("Dec 31, 2023 23:59:59").getTime();
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("cd").innerHTML = days + "n " + hours + "ó "
+  + minutes + "p " + seconds + "mp ";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("cd").innerHTML = "EXPIRED";
+  }
+}, 1000);
