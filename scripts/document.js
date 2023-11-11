e6846c8cc2c591a39b52bab00d20cfdcac0ae8cf
gg3ry <szgery34@gmail.com>
2023-11-11 20:43:46 +0100

    Create document.js

---
+++ scripts/document.js
@@ -0,0 +1,22 @@
+function pdf() {
+    var pdf = document.getElementById("pdf")
+    var docx = document.getElementById("docx")
+    if (pdf.style.display === "none") {
+        pdf.style.display = "block"
+        docx.style.display = "none"
+    }
+    else {
+        pdf.style.display = "none"
+    }
+}
+function docx() {
+    var docx = document.getElementById("docx")
+    var pdf = document.getElementById("pdf")
+    if (docx.style.display === "none") {
+        docx.style.display = "block"
+        pdf.style.display = "none"
+    }
+    else {
+        docx.style.display = "none"
+    }
+}
