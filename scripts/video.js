bda8cd35e7ce4bcd2b366b6cecab0d67f1e892d7
gg3ry <szgery34@gmail.com>
2023-11-17 19:21:24 +0100

    Update video.js

--- scripts/video.js
+++ scripts/video.js
@@ -1,3 +1,51 @@
 function videoselector (videoLink) {
-    document.getElementById('slider').src=videoLink;
+    document.getElementById('video').src=videoLink;
+    document.getElementById('play').style.display="block";
+    document.getElementById('pause').style.display="none";
+    y = 0;
+    v = 2;
+}
+let v = 2;
+let y = 0;
+function pl() {
+    vid = document.getElementById('video')
+    playbutton = document.getElementById('play');
+    pausebutton = document.getElementById('pause');
+    if (y==0)  {
+        vid.pause();
+        playbutton.style.display = 'none';
+        pausebutton.style.display = 'block';
+        y = 1;
+    }
+    else if (y==1) {
+        vid.play();
+        playbutton.style.display = 'block';
+        pausebutton.style.display = 'none';
+        y = 0;
+    }
+}
+function vol() {
+    vid = document.getElementById('video');
+    high = document.getElementById('high');
+    muted = document.getElementById('muted');
+    medium = document.getElementById('medium');
+
+    if (v==2) {
+        high.style.display = 'none';
+        muted.style.display = 'block';
+        v = 0;
+        vid.volume = 0;
+    }
+    else if (y==0) {
+        muted.style.display = 'none';
+        medium.style.display = 'block';
+        v = 1;
+        vid.volume = 0.5;
+    }
+    else {
+        medium.style.display = 'none';
+        high.style.display = 'block';
+        v = 2;
+        vid.volume = 1;
+    }
 }
