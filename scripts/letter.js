5517f578bb055da2f2462391b7b5b3ddebbb44ef
gg3ry <szgery34@gmail.com>
2023-11-17 16:55:02 +0100

    Update letter.js

--- scripts/letter.js
+++ scripts/letter.js
@@ -1,11 +1,12 @@
 function onoff() {
     var x = document.getElementById("letter");
     var y = document.getElementById("doc");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-      y.style.display = "none"
-    } else {
-      x.style.display = "none";
-      y.style.display = "block"
+    if (x.style.display == "none") {
+        x.style.display = "block";
+        y.style.display = "none"
+    } 
+    else {
+        x.style.display = "none";
+        y.style.display = "block"
     }
-  }
+}
