58a4c20646dff5277fe52930af5d277240b6d2c3
gg3ry <szgery34@gmail.com>
2023-11-23 18:24:39 +0100

    Update loading.js

--- scripts/loading.js
+++ scripts/loading.js
@@ -1,3 +1,9 @@
 setTimeout(() => {
     window.location.href = "html/kezdolap.html";
 }, 4000);
+
+
+window.addEventListener('beforeunload', () => {
+  document.body.classList.remove('animate-in'); 
+  document.body.classList.add('animate-out'); 
+}); 
