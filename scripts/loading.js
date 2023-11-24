04735e576bd2caff952b8d03a1d28a72747d57f3
gg3ry <szgery34@gmail.com>
2023-11-24 21:13:06 +0100

    Update loading.js

--- scripts/loading.js
+++ scripts/loading.js
@@ -1,7 +1,11 @@
-setTimeout(() => {
+function tovabb(){
+  document.body.style.backgroundColor = '#306d85';
+  document.querySelector('.center').style.display = 'none';
+  document.querySelector('.sea').style.display = 'block',
+  setTimeout(() => {
     window.location.href = "html/kezdolap.html";
-}, 4000);
-
+  }, 4500);
+}
 
 window.addEventListener('beforeunload', () => {
   document.body.classList.remove('animate-in'); 
